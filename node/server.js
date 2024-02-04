7696655d373f59dc8d22e9921423695a7e6c9327
Tamar Strauss <103378537+Tamar-Strauss@users.noreply.github.com>
2024-02-04 14:42:38 +0200

    Update server.js

--- node/server.js
+++ node/server.js
@@ -1,6 +1,29 @@
+
+const express = require('express');
 const sdk = require('api')('@render-api/v1.0#jw0325lr5hblce');
 
-sdk.auth('rnd_GwZfGE0knyrto5CzeKS6mpnbzMl9');
-sdk.getServices({limit: '20'})
-  .then(({ data }) => console.log(data))
-  .catch(err => console.error(err));
+const app = express();
+
+let PORT = process.env.PORT || 8000;
+
+app.get("/", (req, res) => {
+    sdk.auth('rnd_GwZfGE0knyrto5CzeKS6mpnbzMl9');
+    sdk.getServices({ limit: '20' })
+        .then(({ data }) => {
+            console.log(data);
+            res.json(data);
+        })
+        .catch(err => console.error(err));
+});
+
+app.get("/names", (req, res) => {
+    sdk.auth('rnd_GwZfGE0knyrto5CzeKS6mpnbzMl9');
+    sdk.getServices({ limit: '20' })
+        .then(({ data }) => {
+            console.log(data.map(e => e.service.name));
+            res.json(data.map(e => e.service.name));
+        })
+        .catch(err => console.error(err));
+});
+
+app.listen(PORT, () => { console.log(`Server running on port ${PORT}`); });
