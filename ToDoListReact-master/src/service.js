def70044e7fe0dc41ec83e0e8045259567ced30c
Tamar Strauss <103378537+Tamar-Strauss@users.noreply.github.com>
2024-02-05 23:56:07 +0200

    Update service.js

--- ToDoListReact-master/src/service.js
+++ ToDoListReact-master/src/service.js
@@ -1,3 +1,44 @@
+// import axios from 'axios';
+// axios.defaults.baseURL =process.env.REACT_APP_API_URL;
+// const apiUrl =process.env.REACT_APP_API_URL;
+// var count=0;
+// export default {
+//   getTasks: async () => {
+//     const result = await axios.get(`${apiUrl}/Items`)
+//     return result.data;
+//   },
+
+//   addTask: async (name) => {
+//     console.log('addTask', name)
+//     //TODO
+//     const obj = 
+//       {"id":count++,"name":name,"isComplete":false}
+//     const result = await axios.post(`${apiUrl}/Items`, obj, {
+//       headers: {
+//         'Content-Type': 'application/json'
+//       }
+//     });
+//     return {};
+//   },
+
+//   setCompleted: async (id, isComplete) => {
+//     console.log('setCompleted', { id, isComplete })
+//     const result = await axios.put(`${apiUrl}/Items/${id}`, isComplete, {
+//       headers: {
+//         'Content-Type': 'application/json'
+//       }
+//     });
+//     //TODO
+//     return result;
+//   },
+
+//   deleteTask: async (idd) => {
+//     console.log('deleteTask')
+//     const result = await axios.delete(`${apiUrl}/Items/${idd}`)
+// }
+// }
+
+
 import axios from 'axios';
 axios.defaults.baseURL =process.env.REACT_APP_API_URL;
 const apiUrl =process.env.REACT_APP_API_URL;
@@ -12,7 +53,7 @@ export default {
     console.log('addTask', name)
     //TODO
     const obj = 
-      {"id":count++,"name":name,"isComplete":false}
+      {"id":count++,"name":name,"isComplate":false}
     const result = await axios.post(`${apiUrl}/Items`, obj, {
       headers: {
         'Content-Type': 'application/json'
@@ -29,7 +70,7 @@ export default {
       }
     });
     //TODO
-    return result;
+    return result.data;
   },
 
   deleteTask: async (idd) => {
@@ -37,4 +78,3 @@ export default {
     const result = await axios.delete(`${apiUrl}/Items/${idd}`)
 }
 }
-
